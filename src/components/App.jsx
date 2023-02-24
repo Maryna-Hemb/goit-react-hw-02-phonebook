ccffe03f62d1add0db80836c902337d0b8ba0b8e
Maryna-Hemb <marinagembarskaa@gmail.com>
2023-02-24 21:46:52 +0200

    feat json and add libraries

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+// import { Formik, Field, Form } from 'formik';
+// import { object, string, number, date, InferType } from 'yup';
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout.styled';
+// import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <Layout>
+        <GlobalStyle />
+        React homework template
+      </Layout>
+    );
+  }
+}
